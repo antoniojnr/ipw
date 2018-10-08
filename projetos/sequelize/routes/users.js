a3738bfc521bcc26cdcd093bcac5e42fc2225a13
Antonio Dias <addsjunior@gmail.com>
2018-10-08 07:48:13 -0300

    atualizando projeto sequelize

--- projetos/sequelize/routes/users.js
+++ projetos/sequelize/routes/users.js
@@ -2,11 +2,26 @@ var express = require('express');
 var models = require('../models');
 var bp = require('body-parser');
 var router = express.Router();
+var User = models.User;
 
 router.use(bp.json());
 
-router.get('', function(req, res) {
-  res.send('ok!');
+router.post('', function(req, res) {
+  User.create({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email
+  }).then(function(user) {
+    res.json(user);
+  });
+});
+
+router.get('/:id', function(req, res) {
+  User
+    .findById(req.params.id)
+    .then(function(user) {
+      res.json(user);
+    });
 });
 
 module.exports = router;
