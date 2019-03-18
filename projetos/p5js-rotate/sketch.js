bfa63810828ba67e4ed664964724201d8ba808f9
Antonio Dias <addsjunior@gmail.com>
2019-03-18 18:35:38 -0300

    imagem

--- projetos/p5js-rotate/sketch.js
+++ projetos/p5js-rotate/sketch.js
@@ -1,34 +1,19 @@
-var quads = [];
-var cont = 10;
-var x = 0, y = 0;
 function setup() {
-  createCanvas(501, 501);
-  for (var i = 0; i < 100; i++) {
-    if (i == cont){
-      y += 50
-      x = 0
-      cont += 10
-    }
-     quads.push({
-       x: x,
-       y: y,
-       d: 50,
-     });
-    x += 50
-   }
+createCanvas(500, 640)
+background('#FFE4E1')
 }
-
 function draw() {
-  background(0);
-  for (var i = 0; i <= quads.length - 1; i++){
-    var quad = quads[i]
-    rect(quad.x, quad.y, quad.d, quad.d);
-    if (mouseIsPressed) {
-      if (quad.x + quad.d >= mouseX && quad.x <= mouseX){
-        if (quad.y + quad.d >= mouseY && quad.y <= mouseY){
-          quads.splice(i, 1);
-        }
-      }
-    }
-  }
+translate(width / 2, height / 2);
+fill(&#39;red&#39;);
+ellipse(20, 5 , 40 , 40);
+fill(&#39;#00ff00&#39;)
+rect(0, 30, 40, 40, 5);
+rect(0, 45+30, 40, 40, 5);
+rect(0, 70+50, 40, 40, 5);
+rect(25+20, 30, 40, 40, 5);
+rect(25+20, 45+30, 40, 40, 5);
+rect(25+20, 70+50, 40, 40, 5);
+rect(25+65, 45+30, 40, 40, 5);
+rect(25+20, -15, 40, 40, 5);
+rect(25+65, 45-60, 40, 40, 5);
 }
